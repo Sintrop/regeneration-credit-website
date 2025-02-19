5ebda08da378fa453c3e14342c5744ebd4f9eb26
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-02-19 09:56:40 -0300

    feat: add nav menu mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,6 +3,9 @@ import { Button } from "../ui/button";
 import Image from "next/image";
 import ImageRc from '@/public/assets/img/rc.png';
 import { TType } from "@/types/t";
+import {Sheet, SheetTrigger} from '@/components/ui/sheet';
+import { NavMenuMobile } from "./components/NavMenuMobile";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
 
 interface Props{
     t: TType;
@@ -13,17 +16,17 @@ export async function Header({t}: Props){
             className="container mx-auto py-10 flex items-center justify-between px-5"
         >
             <Link
-                className="flex items-center gap-4"
+                className="flex items-center gap-3"
                 href="/"
             >
                 <Image
                     src={ImageRc}
                     alt="Regeneration credit icon"
                     quality={100}
-                    className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] object-contain"
+                    className="w-[35px] h-[35px] md:w-[50px] md:h-[50px] object-contain"
                 />
 
-                <h1 className="text-white font-bold uppercase lg:text-xl">{t('regenerationCredit')}</h1>
+                <h1 className="text-white font-bold uppercase text-sm md:text-xl">{t('regenerationCredit')}</h1>
             </Link>
 
             <section className="items-center gap-5 hidden lg:flex">
@@ -48,6 +51,15 @@ export async function Header({t}: Props){
                     Launch App
                 </Button>
             </section>
+
+            <div className="lg:hidden">
+                <Sheet>
+                    <SheetTrigger>
+                        <HiOutlineBars3BottomRight size={25} color='white'/>
+                    </SheetTrigger>
+                    <NavMenuMobile t={t} />
+                </Sheet>
+            </div>
         </header>
     )
 }
---
+++ components/Header/components/NavMenuMobile.tsx
@@ -0,0 +1,68 @@
+import { TType } from "@/types/t";
+import {
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import ImageRc from '@/public/assets/img/rc.png';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { FaPlay } from "react-icons/fa6";
+
+interface Props {
+    t: TType;
+}
+export function NavMenuMobile({ t }: Props) {
+    return (
+        <SheetContent className="">
+            <SheetHeader className="">
+                <Link
+                    className="flex justify-start items-center gap-3 w-[150px]"
+                    href="/"
+                >
+                    <Image
+                        src={ImageRc}
+                        alt="Regeneration credit icon"
+                        quality={100}
+                        className="w-8 h-8 object-contain"
+                    />
+
+                    <SheetTitle className="text-black font-bold uppercase text-sm">{t('regenerationCredit')}</SheetTitle>
+                </Link>
+            </SheetHeader>
+
+            <nav className="flex flex-col gap-5 mt-10">
+                <Link
+                    href="/"
+                    className="text-black underline"
+                >
+                    - Home
+                </Link>
+                <Link
+                    href="/resources"
+                    className="text-black underline"
+                >
+                    - Resources
+                </Link>
+            </nav>
+
+            <Button
+                className="px-10 h-[50px] rounded-[40px] bg-blue-primary text-white text-semibold mt-10"
+            >
+                Launch App
+            </Button>
+
+            <Button
+                className="mt-5 border-2 border-blue-primary rounded-[40px] h-[50px] bg-transparent text-black flex items-center justify-between pr-1 font-semibold gap-5 w-fit"
+            >
+                Get started
+
+                <div className="h-[40px] w-[40px] rounded-full bg-blue-primary flex items-center justify-center">
+                    <FaPlay size={20} color='white' />
+                </div>
+            </Button>
+
+        </SheetContent>
+    )
+}
