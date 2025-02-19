9c17bd972c1632d92ca8fae8594cfa20962e35a3
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-02-19 17:05:14 -0300

    feat: add translation what is it for en and pt

--- app/[locale]/(home)/_components/WhatIsIt.tsx
+++ app/[locale]/(home)/_components/WhatIsIt.tsx
@@ -6,28 +6,28 @@ import Image from "next/image";
 interface Props{
     t: TType;
 }
-export async function WhatIsIt({}: Props) {
+export async function WhatIsIt({t}: Props) {
     return (
         <section
             className="flex flex-col items-center justify-between py-10 lg:py-20 lg:flex-row"
         >
             <div className="flex flex-col flex-1 gap-5 w-full lg:gap-10 lg:max-w-[50%]">
-                <h3 className="font-bold text-4xl">Regenerate to earn</h3>
+                <h3 className="font-bold text-4xl">{t('regenerateToEarn')}</h3>
                 <p className="text-xl lg:max-w-[90%]">
-                    The Regeneration Credit is a payment for environmental service system. Users can regenerate to earn the tokens and trade it for a certificate.
+                    {t('descWhatIsIt')}
                 </p>
 
                 <div className="flex flex-col gap-5 md:flex-row md:gap-10">
                     <Button
                         className="w-full h-[50px] rounded-[40px] bg-transparent border-2 border-green-primary text-black md:w-[200px]"
                     >
-                        Download App
+                        {t('downloadApp')}
                     </Button>
 
                     <Button
                         className="w-full h-[50px] rounded-[40px] bg-transparent border-2 border-green-primary text-black md:w-[200px]"
                     >
-                        Web platform
+                        {t('webPlatform')}
                     </Button>
                 </div>
             </div>
@@ -36,7 +36,7 @@ export async function WhatIsIt({}: Props) {
                 <div className="bg-network bg-contain bg-center flex items-center justify-center w-full h-[400px] bg-no-repeat">
                     <Image
                         src={ImageRc}
-                        alt="Regeneration credit icon"
+                        alt={t('altIconRC')}
                         quality={100}
                         className="w-[150px] h-[150px] object-contain"
                     />
