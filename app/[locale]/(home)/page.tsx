5d441181fbbe0ed3f20d747ba8259a3e90f75f10
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-02-19 16:48:28 -0300

    feat: add generate metadata

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -7,8 +7,26 @@ import { RcToken } from './_components/RcToken';
 import { Technology } from './_components/Technology';
 import { Community } from './_components/Community';
 import { Footer } from '@/components/Footer/Footer';
+import type { Metadata } from 'next';
 
 const i18nNamespaces = ['home'];
+
+type Props = {
+    params: Promise<{ locale: string }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateMetadata(
+    { params }: Props
+): Promise<Metadata> {
+    const locale = (await params).locale;
+    const { t } = await initTranslations(locale, i18nNamespaces);
+
+    return {
+        title: 'title',
+        description: 'teste description novo'
+    }
+}
 
 export default async function Home({ params: { locale } }: { params: { locale: string } }) {
     const { t, resources } = await initTranslations(locale, i18nNamespaces);
