185090f123dbbf3bd77912b3107669362be32cea
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-02-20 16:29:06 -0300

    fix: window is not defined

--- app/[locale]/(home)/_components/RcToken/AddTokenToMetamask.tsx
+++ app/[locale]/(home)/_components/RcToken/AddTokenToMetamask.tsx
@@ -4,27 +4,31 @@ import { Button } from "@/components/ui/button";
 import { useTranslation } from "react-i18next";
 
 export function AddTokenToMetamask() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
-    async function addToken(){
-        if(!window.ethereum)return;
-
-        await window.ethereum.request({
-            method: 'wallet_watchAsset',
-            params: {
-                type: "ERC20",
-                options: {
-                    address: process.env.NEXT_PUBLIC_ADDRESS_RCTOKEN,
-                    symbol: process.env.NEXT_PUBLIC_RCTOKEN_SYMBOL,
-                    decimals: 18,
-                    image: process.env.NEXT_PUBLIC_RCTOKEN_IMAGE_URL
+    async function addToken() {
+        if (typeof window !== "undefined") {
+            if (!window.ethereum) return;
+    
+            await window.ethereum.request({
+                method: 'wallet_watchAsset',
+                params: {
+                    type: "ERC20",
+                    options: {
+                        address: process.env.NEXT_PUBLIC_ADDRESS_RCTOKEN,
+                        symbol: process.env.NEXT_PUBLIC_RCTOKEN_SYMBOL,
+                        decimals: 18,
+                        image: process.env.NEXT_PUBLIC_RCTOKEN_IMAGE_URL
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
-    if(!window.ethereum){
-        return <div/>
+    if (typeof window !== "undefined") {
+        if (!window.ethereum) {
+            return <div />
+        }
     }
 
     return (
