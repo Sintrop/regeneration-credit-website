09e5bdaf0f248d6f26a6c0e57ffcd943ffe6b402
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-06-02 14:10:27 -0300

    feat: remove temporarily rc token data

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,63 +1,65 @@
-import initTranslations from '../../i18n';
-import TranslationsProvider from '@/components/TranslationsProvider';
-import { Header } from '@/components/Header/Header';
-import { Hero } from './_components/Hero';
-import { WhatIsIt } from './_components/WhatIsIt';
-import { RcToken } from './_components/RcToken/RcToken';
-import { Technology } from './_components/Technology';
-import { Community } from './_components/Community/Community';
-import { Footer } from '@/components/Footer/Footer';
-import type { Metadata } from 'next';
+import initTranslations from "../../i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
+import { Header } from "@/components/Header/Header";
+import { Hero } from "./_components/Hero";
+import { WhatIsIt } from "./_components/WhatIsIt";
+//import { RcToken } from "./_components/RcToken/RcToken";
+import { Technology } from "./_components/Technology";
+import { Community } from "./_components/Community/Community";
+import { Footer } from "@/components/Footer/Footer";
+import type { Metadata } from "next";
 
-const i18nNamespaces = ['home'];
+const i18nNamespaces = ["home"];
 
 type Props = {
-    params: Promise<{ locale: string }>
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  params: Promise<{ locale: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const locale = (await params).locale;
+  const { t } = await initTranslations(locale, i18nNamespaces);
+
+  return {
+    title: t("seo-title"),
+    description: t("seo-description"),
+  };
 }
 
-export async function generateMetadata(
-    { params }: Props
-): Promise<Metadata> {
-    const locale = (await params).locale;
-    const { t } = await initTranslations(locale, i18nNamespaces);
+export default async function Home({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
 
-    return {
-        title: t('seo-title'),
-        description: t('seo-description'),
-    }
-}
+  return (
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}
+    >
+      <div className="bg-back-home w-full flex flex-col bg-cover bg-center">
+        <Header t={t} />
 
-export default async function Home({ params: { locale } }: { params: { locale: string } }) {
-    const { t, resources } = await initTranslations(locale, i18nNamespaces);
+        <Hero t={t} />
+      </div>
 
-    return (
-        <TranslationsProvider
-            namespaces={i18nNamespaces}
-            locale={locale}
-            resources={resources}
-        >
-            <div className='bg-back-home w-full flex flex-col bg-cover bg-center'>
-                <Header t={t} />
+      <main>
+        <div className="container mx-auto px-5 lg:px-20">
+          <WhatIsIt t={t} />
 
-                <Hero t={t} />
-            </div>
+          {/* <RcToken t={t} /> */}
+        </div>
 
-            <main>
-                <div className='container mx-auto px-5 lg:px-20'>
-                    <WhatIsIt t={t} />
+        <Technology t={t} />
 
-                    <RcToken t={t} />
-                </div>
+        <div className="container mx-auto px-5 lg:px-20">
+          <Community t={t} />
+        </div>
+      </main>
 
-                <Technology t={t} />
-
-                <div className='container mx-auto px-5 lg:px-20'>
-                    <Community t={t} />
-                </div>
-            </main>
-
-            <Footer t={t} />
-        </TranslationsProvider>
-    );
+      <Footer t={t} />
+    </TranslationsProvider>
+  );
 }
