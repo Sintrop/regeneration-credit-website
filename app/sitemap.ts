065797ac9dd7a04d8c690ed999cecb1cf0067cbe
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-06-12 09:21:12 -0300

    feat: add tutorials page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,42 @@
-import type {MetadataRoute} from 'next';
+import type { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap{
-    return [
-        {
-            url: 'https://regenerationcredit.com',
-            lastModified: new Date(),
-            changeFrequency: 'daily',
-            priority: 1,
-            alternates: {
-                languages: {
-                    en: 'https://regenerationcredit.com',
-                    pt: 'https://regenerationcredit.com/pt'
-                }
-            }
-        },
-        {
-            url: 'https://regenerationcredit.com/resources',
-            lastModified: new Date(),
-            changeFrequency: 'daily',
-            priority: 0.8,
-            alternates: {
-                languages: {
-                    en: 'https://regenerationcredit.com/resources',
-                    pt: 'https://regenerationcredit.com/pt/resources'
-                }
-            }
-        },
-    ]
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://regenerationcredit.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+      alternates: {
+        languages: {
+          en: 'https://regenerationcredit.com',
+          pt: 'https://regenerationcredit.com/pt'
+        }
+      }
+    },
+    {
+      url: 'https://regenerationcredit.com/resources',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: 'https://regenerationcredit.com/resources',
+          pt: 'https://regenerationcredit.com/pt/resources'
+        }
+      }
+    },
+    {
+      url: 'https://regenerationcredit.com/tutorials',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: 'https://regenerationcredit.com/tutorials',
+          pt: 'https://regenerationcredit.com/pt/tutorials'
+        }
+      }
+    },
+  ]
 }
